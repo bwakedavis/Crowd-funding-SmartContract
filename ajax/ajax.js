1615a48abe6f459507881f258eec78d249b75dc7
bwakedavis <bwakedavis@gmail.com>
2020-03-13 19:22:58 +0300

    woking with ajax and JSON

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -10,20 +10,44 @@ function loadFile(){
     let xhr = new XMLHttpRequest();
     // Open the file
     xhr.open('GET', './file.txt', true);
+    // console.log(xhr.readyState)
 
-    // xhr.onload = function(){
-    //     if(this.status == 200){
-    //         console.log(this.responseText)
-    //     }
-    // }
+    // loaders (optional)
+    xhr.onprogress=function(){
+        console.log(this.readyState)
+    }
+
+    xhr.onload = function(){
+        if(this.status == 200){
+            // console.log(this.responseText)
+            document.getElementById('text-file').innerHTML = this.responseText
+        }else{
+            document.getElementById('text-file').innerHTML = "Not FOUnd"
+        }
+    }
+
+    xhr.onerror = function(){
+        console.log("an error occured")
+    }
     //Using onreadystatechange
         // HTTP STATUSES
         //200: "OK"
         //403: "Fobiden"
         //404: "Not Found"
-    xhr.onreadystatechange = function(){
-        if()
-    }
+        //READY STATE VALUES
+        //0:"Request not initialized"
+        //1: "server connection established"
+        //2: "request received"
+        //3: "processing request"
+        //4: "Request finished and response is ready"
+
+    // xhr.onreadystatechange = function(){
+    //     console.log(xhr.readyState)
+    //     if(this.readyState == 4 && this.status == 200){
+    //         console.log(this.responseText)
+    //     }
+    // }
+
     //sends request
     xhr.send()
 }
