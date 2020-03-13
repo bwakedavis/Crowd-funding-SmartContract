dcaf504bf219868c74d90bc6d593e22a66997c41
bwakedavis <bwakedavis@gmail.com>
2020-03-14 01:15:30 +0300

    working with json and fetch challenfing but worth it

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -9,3 +9,76 @@ function fetchText(){
         console.log(err)
     })
 }
+
+
+document.getElementById("fetch-user").addEventListener('click', fetchUser)
+
+function fetchUser(){
+    fetch('user.json').then((res)=>{
+        return res.json()
+    }).then((user)=>{
+        let output = `
+        <ul>
+            <li>${user.age}</li>
+            <li>${user.name}</li>
+            <li>${user.email}</li>
+        </ul>
+        `
+
+        document.getElementById('user').innerHTML = output
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+
+
+document.getElementById("fetch-users").addEventListener('click', fetchUsers)
+
+function fetchUsers(){
+    fetch('users.json').then((res)=>{
+        return res.json()
+    }).then((users)=>{
+        output = ""
+
+        users.forEach(user => {
+            output += `
+             <ul>
+                <li>${user.age}</li>
+                <li>${user.name}</li>
+                <li>${user.email}</li>
+             </ul>
+             `   
+        });
+             
+
+        document.getElementById('users').innerHTML = output
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+document.getElementById("fetch-lorem-text").addEventListener('click', fetchLorem)
+
+function fetchLorem(){
+    fetch('').then((res)=>{
+        return res.json()
+    }).then((users)=>{
+        output = ""
+
+        users.forEach(user => {
+            output += `
+             <ul>
+                <li>${user.age}</li>
+                <li>${user.name}</li>
+                <li>${user.email}</li>
+             </ul>
+             `   
+        });
+             
+
+        document.getElementById('display-lorem-text').innerHTML = output
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
