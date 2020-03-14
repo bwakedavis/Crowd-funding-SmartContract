d8bb6fab88362ffeb12280389addc3d0d8e1fe90
bwakedavis <bwakedavis@gmail.com>
2020-03-14 17:58:46 +0300

    foreach with arrays

---
+++ highOrderArrayMethods/arrayMetods.js
@@ -0,0 +1,14 @@
+const companies = [
+    {name:"Company One", category:"Finance", start: 1981, end: 2003},
+    {name:"Company Two", category:"Retail", start: 1981, end: 2003},
+    {name:"Company Three", category:"Auto", start: 1981, end: 2003},
+    {name:"Company Four", category:"Retail", start: 1981, end: 2003},
+    {name:"Company Five", category:"Technology, start: 1981, end: 2003},
+    {name:"Company Six", category:"Finance", start: 1981, end: 2003},
+    {name:"Company Seven", category:"Auto", start: 1981, end: 2003},
+    {name:"Company Eight", category:"Retail", start: 1981, end: 2003},
+    {name:"Company Nine", category:"Technology", start: 1981, end: 2003},
+    {name:"Company Ten", category:"Finance", start: 1981, end: 2003},
+]
+
+const numbers = [45,64,12,56,24,99,76,9,35,71,17,28,83,40]
