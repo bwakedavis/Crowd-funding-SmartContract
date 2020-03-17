5d1dd188d2675ffa22db5ad49642f416f6c679dd
bwakedavis <bwakedavis@gmail.com>
2020-03-17 11:02:56 +0300

    wrote a js function to animate the linkicon with codes

--- animatedIcons/animate icons.js
+++ animatedIcons/animate icons.js
@@ -1,3 +1,17 @@
-function breakChain(){
-    document.getElementById('chain').innerHTML = 
-}
+    // Brake Chain Animation
+    function brakeChain(){
+        let chain = document.getElementById('chain');
+        chain.innerHTML = "&#xf0c1;";
+  
+        setTimeout(function(){
+          chain.innerHTML = "&#xf127;";
+        }, 1000);
+      }
+  
+      // Call Animation
+      brakeChain();
+  
+      // Animate Every 2 Seconds
+      setInterval(brakeChain, 2000);
+  
+      // Bat
