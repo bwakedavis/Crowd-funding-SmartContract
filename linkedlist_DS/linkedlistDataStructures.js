b46c7037196db5dcb0bdb3785f90d4d96b669008
bwakedavis <bwakedavis@gmail.com>
2020-03-24 00:15:31 +0300

    clear coverage on linked list in js

--- linkedlist_DS/linkedlistDataStructures.js
+++ linkedlist_DS/linkedlistDataStructures.js
@@ -1,61 +1,145 @@
-class Node{
-    constructor(data, next = null){
-        this.data = data
-        this.next = next
+// Construct Single Node
+class Node {
+    constructor(data, next = null) {
+      this.data = data;
+      this.next = next;
     }
-}
-
-class LinkedList{
-    constructor(){
-        this.head = null;
-        this.size = 0;
+  }
+  
+  // Create/Get/Remove Nodes From Linked List
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.size = 0;
     }
-
-    //Insert first Node
-    insertFirst(data){
-
-        this.head = new Node(data, this.head)
-        this.size++
+  
+    // Insert first node
+    insertFirst(data) {
+      this.head = new Node(data, this.head);
+      this.size++;
     }
-    //Insert last Node
-    insertLast(data){
-        let node = new Node(data)
-        let current;
-        //if empty make head
-        if(!this.head){
-            this.head = node;
-        }else{
-            current = this.head
-
-            while(current.next){
-                current = current.next
-            }
-            current.next = node
+  
+    // Insert last node
+    insertLast(data) {
+      let node = new Node(data);
+      let current;
+  
+      // If empty, make head
+      if (!this.head) {
+        this.head = node;
+      } else {
+        current = this.head;
+  
+        while (current.next) {
+          current = current.next;
         }
-
+  
+        current.next = node;
+      }
+  
+      this.size++;
     }
-    //Insert at Index
-    
-    //Get at Index
-
-    //Remove at Index
-
-    //Clear at Index
-
-    //Print list data
-    printListData(){
-        let current = this.head;
-        while(current){
-            console.log(current.data)
-            current = current.next
+  
+    // Insert at index
+    insertAt(data, index) {
+      //  If index is out of range
+      if (index > 0 && index > this.size) {
+        return;
+      }
+  
+      // If first index
+      if (index === 0) {
+        this.insertFirst(data);
+        return;
+      }
+  
+      const node = new Node(data);
+      let current, previous;
+  
+      // Set current to first
+      current = this.head;
+      let count = 0;
+  
+      while (count < index) {
+        previous = current; // Node before index
+        count++;
+        current = current.next; // Node after index
+      }
+  
+      node.next = current;
+      previous.next = node;
+  
+      this.size++;
+    }
+  
+    // Get at index
+    getAt(index) {
+      let current = this.head;
+      let count = 0;
+  
+      while (current) {
+        if (count == index) {
+          console.log(current.data);
         }
+        count++;
+        current = current.next;
+      }
+  
+      return null;
     }
-}
-
-const ll = new LinkedList()
-
-ll.insertFirst(100)
-ll.insertFirst(200)
-ll.insertFirst(300)
-ll.insertLast(400)
-ll.printListData()
+  
+    // Remove at index
+    removeAt(index) {
+      if (index > 0 && index > this.size) {
+        return;
+      }
+  
+      let current = this.head;
+      let previous;
+      let count = 0;
+  
+      // Remove first
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        while (count < index) {
+          count++;
+          previous = current;
+          current = current.next;
+        }
+  
+        previous.next = current.next;
+      }
+  
+      this.size--;
+    }
+  
+    // Clear list
+    clearList() {
+      this.head = null;
+      this.size = 0;
+    }
+  
+    // Print list data
+    printListData() {
+      let current = this.head;
+  
+      while (current) {
+        console.log(current.data);
+        current = current.next;
+      }
+    }
+  }
+  
+  const ll = new LinkedList();
+  
+  ll.insertFirst(100);
+  ll.insertFirst(200);
+  ll.insertFirst(300);
+  ll.insertLast(400);
+  ll.insertAt(500, 3);
+  
+  // ll.clearList();
+  // ll.getAt(2);
+  
+  ll.printListData();
