ef6990e633a395cd708ef9fbb4720166c93ac829
bwakedavis <bwakedavis@gmail.com>
2020-04-02 00:11:19 +0300

    basic of if statement

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 //declare variables with var,let,const
 
-let name = "davis"; //string
+let f_name = "davis"; //string
+l_name = 'bwake'
 let id = 2345; //number
-let alive = true; //boolean
+let cooler = true; //boolean
 let xyz;
+let age = 20;
+
 //re-assign variables with new values
 id = 5432;
 xyz = null //nothing
 let fiveDs;
 // console.log(name,id,alive,xyz);
 fiveDs = ['Davis',"Dan","Dick","Don","dave"]
-name += ' bwake'
-console.log(name)
-id +=2
-console.log(id)
-id -=2
-console.log(id)
-id /=2
-console.log(id)
-id *=2
-console.log(id)
-id %=2
-console.log(id)
+// name += ' bwake'
+// console.log(name)
+// id +=2
+// console.log(id)
+// id -=2
+// console.log(id)
+// id /=2
+// console.log(id)
+// id *=2
+// console.log(id)
+// id %=2
+// console.log(id)
 
-console.log(fiveDs);
-console.log(fiveDs.length);
-console.log(fiveDs[0] + " aka " + fiveDs[4])
+// console.log(fiveDs);
+// console.log(fiveDs.length);
+// console.log(fiveDs[0] + " aka " + fiveDs[4])
+
+//falsey - false,null,undefined,0,"",'',Nan
+if(age == 20){
+    console.log("you're 20")
+}else{
+    console.log("you're not 20")
+}
 
