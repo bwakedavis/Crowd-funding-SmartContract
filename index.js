953a751dcf06b28be9855c88589d8df6d85cbc2c
bwakedavis <bwakedavis@gmail.com>
2020-04-02 00:05:07 +0300

    basics of primitive and complex datatypes js

--- index.js
+++ index.js
@@ -3,4 +3,27 @@
 let name = "davis"; //string
 let id = 2345; //number
 let alive = true; //boolean
+let xyz;
+//re-assign variables with new values
+id = 5432;
+xyz = null //nothing
+let fiveDs;
+// console.log(name,id,alive,xyz);
+fiveDs = ['Davis',"Dan","Dick","Don","dave"]
+name += ' bwake'
+console.log(name)
+id +=2
+console.log(id)
+id -=2
+console.log(id)
+id /=2
+console.log(id)
+id *=2
+console.log(id)
+id %=2
+console.log(id)
+
+console.log(fiveDs);
+console.log(fiveDs.length);
+console.log(fiveDs[0] + " aka " + fiveDs[4])
 
